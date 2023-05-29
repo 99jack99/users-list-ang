50aff397e3cbf22a98c311bf8d32a99cc41cf016
9jack9 <jackson55adl@gmail.com>
2023-05-29 20:30:37 +0200

    adding already done pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './views/register/register.component';
+import { UsersComponent } from './views/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegisterComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,

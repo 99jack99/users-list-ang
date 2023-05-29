32651e7487be062739d6277a3c5ae008ceec4476
9jack9 <jackson55adl@gmail.com>
2023-05-29 20:45:33 +0200

    first api call, get users

--- src/app/views/users/users.component.ts
+++ src/app/views/users/users.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from "axios";
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
+  /* users:Array<T>= "hola";  */
+
+  /* hola = "asd" */
+
+  ngOnInit(): void {
+    
+    axios.get('https://reqres.in/api/users')
+    .then((res)=>{
+      console.log(res);
+    })
+
+    .catch((res)=>{
+      console.log(res);
+    })
+  }
 }
